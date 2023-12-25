68c58b97e2acd947bd70f4f56d8dcb8da9fc314e
esma <esmaaguldogan@gmail.com>
2023-12-25 14:33:20 +0300

    first commit

---
+++ app/src/main/java/com/esma/myapplicationkotlin/Deneme.kt
@@ -0,0 +1,27 @@
+package com.esma.myapplicationkotlin
+
+
+fun main(){
+    var ogrenciAdi = "esma"
+    var ogrenciYasi = 15
+    var ogrenciBoyu = 1.98
+    var ogrenciBasHarf = 'e'
+
+    println(ogrenciAdi)
+    println(ogrenciYasi)
+    println(ogrenciBoyu)
+    println(ogrenciBasHarf)
+
+    var urun_id:Int = 3416
+    var urun_adi:String = "Kol saati"
+    var urun_adet:Int = 100
+    var urun_fiyat:Double = 109.99
+    var urun_tedarikci:String = "rolex"
+
+    println(urun_id)
+    println(urun_adi)
+    println(urun_adet)
+    println(urun_fiyat)
+    println(urun_tedarikci)
+
+}
