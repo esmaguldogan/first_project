c8ab2475164cd4b2b809ecab7d02acaad31f8eea
esma <esmaaguldogan@gmail.com>
2024-01-02 23:21:44 +0300

    Konsol girdisini çok anlamadım :(

---
+++ app/src/main/java/com/esma/myapplicationkotlin/KonsolGirdisi.kt
@@ -0,0 +1,17 @@
+package com.esma.myapplicationkotlin
+
+import java.util.Scanner
+
+fun main() {
+
+    println("Giriniz")
+
+    val girdi = Scanner(System.`in`)
+    /* val ad = girdi.next() */
+
+    val sayi = girdi.nextInt()
+    println("Sonuc: $sayi")
+
+
+
+}
---
+++ app/src/main/java/com/esma/myapplicationkotlin/Turdonusumu.kt
@@ -0,0 +1,21 @@
+package com.esma.myapplicationkotlin
+
+class Turdonusumu
+ fun main() {
+
+ var i:Int = 47
+ var f:Float = 42.89f
+ var d:Double = 42.45
+
+  var sonuc1: Int = d.toInt()
+  var sonuc2: Double = i.toDouble()
+  var sonuc3: Float = f.toFloat()
+     println(sonuc1)
+
+  var str1 = i.toString()
+  println(str1)
+
+  // Tür dönüştürmek için ifadeleri tanımlayıp "ifade.to(döüştürmek istediğin tür) yapmak yeterli"
+
+
+}
