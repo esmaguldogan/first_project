5e67c1853f96cf87a6798eaed52c4e701a1b274d
esma <esmaaguldogan@gmail.com>
2024-01-09 20:16:00 +0300

    Öğrenilen if yapısıyla vücüt kitle indeksi hesaplayan program yazıldı

---
+++ app/src/main/java/com/esma/myapplicationkotlin/Vucutkitleindeksi.kt
@@ -0,0 +1,34 @@
+package com.esma.myapplicationkotlin
+
+import java.util.Scanner
+fun main(){
+    val girdi = Scanner(System.`in`)
+    println("Boyunuzu cm cinsinden giriniz")
+
+    val boy = girdi.nextInt()
+    println("Boyunuz : $boy")
+
+    println("Kilonuzu giriniz")
+
+    val kilo = girdi.nextInt()
+    println("Kilonuz : $kilo")
+
+    val boym = boy / 100
+
+    val vucutki = kilo / (boym*boym)
+
+    println ("Vücut kitle indeksi: $vucutki")
+
+    if (vucutki<20)
+        println("Ortalamaya göre zayıfsınız :(")
+    else if (vucutki<=25)
+        println("Ortalama kilodasınız")
+    else if(vucutki<=30)
+        println("Ortamaya göre fazla kilolusunuz")
+    else
+        println("Obezsiniz")
+
+
+
+
+}
