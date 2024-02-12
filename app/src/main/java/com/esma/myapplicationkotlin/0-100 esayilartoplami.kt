e6a6df733a13fe3170c158199ecc577fb2c55b50
esma <esmaaguldogan@gmail.com>
2024-02-12 22:07:22 +0300

    0 dan 100 e sayılar toplamı ödevi 99 a gelince sona ulaştınız

--- app/src/main/java/com/esma/myapplicationkotlin/0-100 esayilartoplami.kt
+++ app/src/main/java/com/esma/myapplicationkotlin/0-100 esayilartoplami.kt
@@ -1,11 +1,26 @@
 package com.esma.myapplicationkotlin
 
 fun main() {
+    //1. şekilde
 
-    var sayilartoplami= 0
+    var sayilartoplami = 0
 
-    for(i in 1..100) {
+    for (i in 1..100) {
         println("Sayılar Toplamı: $sayilartoplami")
         sayilartoplami = sayilartoplami + i
+
+        if (sayilartoplami==99)
+            println("Sona ulaştınız")
+    }
+    //2.şekilde
+
+    for(i in 1..100){
+        println(i)
+
+        if (i==99)
+        println("Sona ulaştınız")
     }
 }
+
+
+
